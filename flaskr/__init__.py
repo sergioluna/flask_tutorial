f29bbb03b6872d89768ef93663480a9cb5ad8456
sergioluna <sluna@sergioluna.dev>
2022-01-12 12:06:46 -0800

    Create Flask appilcation 'flaskr'

---
+++ flaskr/__init__.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    """ Application Factory """
+    app = Flask(__name__, instance_relative_config=True)
+
+    # Secret key should be overwritten for deployment
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    # create instance folder if it doesn't exist
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
