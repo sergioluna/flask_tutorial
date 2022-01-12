37c29dfd3c847e210b6ed6b835d2528763afba6c
sergioluna <sluna@sergioluna.dev>
2022-01-12 15:11:25 -0800

    Fix index page bugs

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -14,10 +14,10 @@ def index():
     db = get_db()
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.auther_id = user.id'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('blog/index/html', posts=posts)
+    return render_template('blog/index.html', posts=posts)
 
 
 @bp.route('/create', methods=['GET', 'POST'])
